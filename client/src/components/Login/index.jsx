ce581d6ad4f1c3aeda7ea22c770eed16f10ae04b
Mark Carson <markcarson121@gmail.com>
2023-08-03 12:29:30 +0200

    created AUTH_URL

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -1,4 +1,5 @@
-const AUTH_URL = "https://accounts.spotify.com/authorize";
+const AUTH_URL =
+  "https://accounts.spotify.com/authorize?client_id=774e2a6f86984ac2ae34979fbc1fa412&response_type=code&redirect_uri=http://localhost:5173/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 export function Login() {
   return <div>Login</div>;
