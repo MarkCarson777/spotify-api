3e0417b360039f2159a0fcdbd590b56dfdafe923
Mark Carson <markcarson121@gmail.com>
2023-08-03 12:43:54 +0200

    fixed AUTH_URL typo

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -1,6 +1,10 @@
 const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=774e2a6f86984ac2ae34979fbc1fa412&response_type=code&redirect_uri=http://localhost:5173/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+  "https://accounts.spotify.com/authorize?client_id=774e2a6f86984ac2ae34979fbc1fa412&response_type=code&redirect_uri=http://localhost:5173&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 export function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <a href={AUTH_URL}>Login With Spotify</a>
+    </div>
+  );
 }
