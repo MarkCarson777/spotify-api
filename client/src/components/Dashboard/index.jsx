c74b20bd2549f865152a9246d3fa6161af4ad01d
Mark Carson <markcarson121@gmail.com>
2023-08-08 16:26:17 +0200

    added input to dashboard and installed spotify web api node in client

--- client/src/components/Dashboard/index.jsx
+++ client/src/components/Dashboard/index.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 import { useAuth } from "../../hooks/useAuth";
 
 export function Dashboard({ code }) {
+  const [search, setSearch] = useState("");
   const accessToken = useAuth(code);
 
-  return <>{code}</>;
+  return (
+    <form>
+      <input
+        className="border border-red-500"
+        type="text"
+        value={search}
+        placeholder="Search..."
+      />
+    </form>
+  );
 }
