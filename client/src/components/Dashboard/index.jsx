e7efaed83fd1d805594ba0fa0858f0a36b97c3b7
Mark Carson <markcarson121@gmail.com>
2023-08-08 16:34:09 +0200

    retrieved data from Spotify api

--- client/src/components/Dashboard/index.jsx
+++ client/src/components/Dashboard/index.jsx
@@ -1,10 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { useAuth } from "../../hooks/useAuth";
 
+import SpotifyWebApi from "spotify-web-api-node";
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: "5b520526277442868e441edb991d633f",
+});
+
 export function Dashboard({ code }) {
   const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
   const accessToken = useAuth(code);
+
+  useEffect(() => {
+    if (!accessToken) return;
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
+
+  useEffect(() => {
+    if (!search) return setSearchResults([]);
+    if (!accessToken) return;
+
+    spotifyApi.searchTracks(search).then((res) => {
+      console.log(res);
+    });
+  }, [search, accessToken]);
 
   return (
     <form>
@@ -12,6 +33,7 @@ export function Dashboard({ code }) {
         className="border border-red-500"
         type="text"
         value={search}
+        onChange={(e) => setSearch(e.target.value)}
         placeholder="Search..."
       />
     </form>
