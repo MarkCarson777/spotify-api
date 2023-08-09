375c77535c6f7428176c4e95e1aada1b0856c449
Mark Carson <markcarson121@gmail.com>
2023-08-09 15:51:02 +0200

    added basic styling to Playlist component

--- client/src/components/Playlist/index.jsx
+++ client/src/components/Playlist/index.jsx
@@ -1,3 +1,19 @@
 export function Playlist({ playlist }) {
-  return <div>{playlist.name}</div>;
+  return (
+    <div className="flex items-center p-2 border w-fit rounded-lg">
+      <img
+        className="rounded-md mr-2"
+        src={playlist.images[2].url}
+        alt={playlist.name}
+      />
+      <div>
+        <span>{playlist.name}</span>
+        <div>
+          <span>Playlist</span>
+          <span>&nbsp;&#8226;&nbsp;</span>
+          <span>{playlist.owner.display_name}</span>
+        </div>
+      </div>
+    </div>
+  );
 }
