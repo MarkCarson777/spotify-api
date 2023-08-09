b4d5de022a83f148a1b9436906f15078bf291283
Mark Carson <markcarson121@gmail.com>
2023-08-09 15:36:49 +0200

    installed prop-types

--- client/src/containers/Playlists/index.jsx
+++ client/src/containers/Playlists/index.jsx
@@ -1,4 +1,10 @@
+import PropTypes from "prop-types";
+
 export function Playlists({ playlists }) {
   console.log(playlists);
   return <div>Playlists</div>;
 }
+
+Playlists.propTypes = {
+  playlists: PropTypes.array.isRequired,
+};
