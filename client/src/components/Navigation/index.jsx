f13fb4cc97ec1032ea298e00bbc687cc9ce76244
Mark Carson <markcarson121@gmail.com>
2023-08-10 11:30:03 +0200

    integrated classanmes into Navigation and applied tailwind classes

--- client/src/components/Navigation/index.jsx
+++ client/src/components/Navigation/index.jsx
@@ -1,10 +1,11 @@
-import PropTypes from "prop-types";
-
+// Third Party
+import classnames from "classnames";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 export function Navigation({ className }) {
   return (
-    <div className={className}>
+    <div className={classnames("flex justify-around", className)}>
       <Link to="">Feature One</Link>
       <Link to="">Feature Two</Link>
       <Link to="">Feature Three</Link>
